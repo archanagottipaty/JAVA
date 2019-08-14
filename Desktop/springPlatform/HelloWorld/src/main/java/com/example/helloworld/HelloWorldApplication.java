a277f1232d415ef5568e6c5c395d66aa5aabd0a9
archanagottipaty <archana.gottipaty@gmail.com>
2019-08-14 11:59:40 -0700

    path

--- Desktop/springPlatform/HelloWorld/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ Desktop/springPlatform/HelloWorld/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,29 +1,30 @@
 package com.example.helloworld;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 
+@SpringBootApplication
 @RestController
 public class HelloWorldApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+	SpringApplication.run(HelloWorldApplication.class, args);
+//		SpringApplication.run(HomeController1.class, args);
 	}
-
 	@RequestMapping("/")
-	
 	public String hello() {
 		return "Hello client! How are you doing?";
 	}
 	
 	@RequestMapping("/random")
-	public String random() {
-		
+	public String random() {	
 		return "Spring Boot is great! So easy to just respond with strings";
 		
 	}
+	
+	@RequestMapping("dojo/m/{track}/{module}/{lesson}")
+    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
+    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;}
 }
---
+++ Desktop/springPlatform/HelloWorld/src/main/java/com/example/helloworld/dojo.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/dojo")
+public class dojo {
+	@RequestMapping("/m/{track}/{module}/{lesson}")
+    public String showLesson(@PathVariable("track") String track, @PathVariable("module") String module, @PathVariable("lesson") String lesson){
+    	return "Track: " + track + ", Module: " + module + ", Lesson: " + lesson;}
+	
+	
+
+}
