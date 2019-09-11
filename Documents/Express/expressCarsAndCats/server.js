5e200e0804610c503628f1466a78cb4c39dc869c
archanagottipaty <archana.gottipaty@gmail.com>
2019-09-10 17:27:23 -0700

    added changes for localhost:8000 to work by moving one line to top of server.js

--- Documents/Express/expressCarsAndCats/server.js
+++ Documents/Express/expressCarsAndCats/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + "/static"));
 app.get("/users", (req, res) => {
 })
 
@@ -9,5 +9,4 @@ app.get('/', (request, response) => {
 });
 app.listen(8000, () => console.log("listening on port 8000"));
 
-app.use(express.static(__dirname + "/static"));
 
