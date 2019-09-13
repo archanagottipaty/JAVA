0871aa7649115320156ed3d26b3730b32607a189
archanagottipaty <archana.gottipaty@gmail.com>
2019-09-12 17:52:17 -0700

    adding the incomplete EJS Cat Data, I only wrote the route for the first cat

--- Documents/Express/EJSCarsandCats/server.js
+++ Documents/Express/EJSCarsandCats/server.js
@@ -2,20 +2,24 @@ const express = require("express");
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/static"));
-app.get('/', (request, response) => {
+app.get('/', (request, res) => {
 res.render('index');
 });
-
-app.get('/cars', (request, res) => {
- res.render('cars');
-});
-
 app.get('/cats', (request, res) => {
    res.render('cats');
 });
 
-app.get('/cars/new', (request, res) => {
-  res.render('form'); 
+app.get('/cuddles', (request,res) => {
+ var cat_array = [
+        {picture: "cat.jpg",favfood: "Tuna", age:3, sleepingspots:" under the bed"} 
+    ];
+	res.render('cuddles',{cuddles: cat_array});
+});
+app.get('/cuddles1', (request,res) => {
+        res.render('cuddles');
+});
+app.get('/cuddles2', (request,res) => {
+        res.render('cuddles');
 });
 app.listen(8000, () => console.log("listening on port 8000"));
 
